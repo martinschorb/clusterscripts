5c08ecaf53b7122c5cef8a98b5ca6de63fb64261
Martin Schorb <martin.schorb@embl.de>
2018-05-17 16:32:44 +0200

    runs with single frame tomos on the cluster

--- tomo_submit.sh
+++ tomo_submit.sh
@@ -12,13 +12,11 @@ inputdir="$2";
 directive="$3";
 patchtrack="$4";
 
-TMPDIR=test
-
-
 numcpu=12
 
-#module load IMOD
-#source $EBROOTIMOD/IMOD-linux.sh
+module load IMOD
+
+source $EBROOTIMOD/IMOD-linux.sh
 
 tomouser=`whoami`;
 
@@ -30,21 +28,23 @@ ext1=${extension,,}
 
 sfile=$inputdir/$inputfile
 
- #cp $sfile $TMPDIR/
- #cp $directive $TMPDIR/
+cp $sfile $TMPDIR/
+cp $directive $TMPDIR/
 
- cd $TMPDIR
- 
- loc_dir=`ls *.adoc`
- 
+cd $TMPDIR
+
  if [ $patchtrack ]
  then
- python /g/emcf/schorb/code/cluster/patchtomo.py -u `whoami` $sfile $loc_dir $numcpu
+ python /g/emcf/schorb/code/cluster/patchtomo.py -u `whoami` $inputfile $directive $numcpu
  else 
  batchruntomo -root $base -directive $directive -current . -em $tomouser@embl.de -cp $numcpu
  fi
  
  
  rfile=$TMPDIR/${base}.rec
+ cp $rfile $inputdir/ 
+ 
+ cat etomo_err_*.log >> $inputdir/"$base"-"$SLURM_JOBID"_err.log
+ cp batchruntomo.log $inputdir/"$base"-"$SLURM_JOBID".log
 
- cp $rfile $inputdir/
+
