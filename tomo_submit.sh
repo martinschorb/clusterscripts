1962db0c2a4906baa9c872a501ba3ce25f149614
Martin Schorb <martin.schorb@embl.de>
2018-05-18 16:45:55 +0200

    ran all Pedros datasets including montages

--- tomo_submit.sh
+++ tomo_submit.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1                        # number of nodes
 #SBATCH -n 12                        # number of cores
 #SBATCH --mem 12G                  # memory pool for all cores
-#SBATCH -t 0-00:15:00                   # runtime limit (D-HH:MM:SS)
+#SBATCH -t 0-00:30:00                   # runtime limit (D-HH:MM:SS)
 #SBATCH --mail-type=FAIL 
 #SBATCH --mail-user=schorb@embl.de 
  
@@ -18,6 +18,7 @@ module load IMOD
 
 source $EBROOTIMOD/IMOD-linux.sh
 
+
 tomouser=`whoami`;
 
 extension=$([[ "$inputfile" = *.* ]] && echo ".${inputfile##*.}" || echo '')
@@ -31,12 +32,17 @@ sfile=$inputdir/$inputfile
 cp $sfile $TMPDIR/
 cp $directive $TMPDIR/
 
+loc_dir=$(basename "${directive}")
+
 cd $TMPDIR
 
- if [ $patchtrack ]
+ if [ $patchtrack = m ];
  then
- python /g/emcf/schorb/code/cluster/patchtomo.py -u `whoami` $inputfile $directive $numcpu
- else 
+ python /g/emcf/schorb/code/cluster/patchtomo.py -u `whoami` -m $inputfile $loc_dir $numcpu
+ elif [ $patchtrack ];
+ then
+ python /g/emcf/schorb/code/cluster/patchtomo.py -u `whoami` $inputfile $loc_dir $numcpu
+ else
  batchruntomo -root $base -directive $directive -current . -em $tomouser@embl.de -cp $numcpu
  fi
  
@@ -44,7 +50,7 @@ cd $TMPDIR
  rfile=$TMPDIR/${base}.rec
  cp $rfile $inputdir/ 
  
- cat etomo_err_*.log >> $inputdir/"$base"-"$SLURM_JOBID"_err.log
- cp batchruntomo.log $inputdir/"$base"-"$SLURM_JOBID".log
+ cat etomo_err_*.log >> $inputdir/"$base-$SLURM_JOBID _err.log"
+ cp batchruntomo.log $inputdir/"$base-$SLURM_JOBID .log"
 
 
