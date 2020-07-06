606c07e3dc578770a91b15bff4ee70fcad7dad30
Martin Schorb <martin.schorb@embl.de>
2020-07-06 17:25:15 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+ # -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='cluster_ms',
+      version='20200706',
+      py_modules=['submit_slurm'],
+      description='Tools to interact with BigDataViewer.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Martin Schorb',
+      author_email='schorb@embl.de',
+      license='GPLv3',
+      install_requires=[
+      'os'
+      ],
+      )
