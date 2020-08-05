d3dea3144b9675ffc4ab971650b514520b88ca34
Martin Schorb <martin.schorb@embl.de>
2020-08-05 17:34:02 +0200

    fiji test

--- fiji_gui.py
+++ fiji_gui.py
@@ -14,38 +14,40 @@ import os
 def calculate(*args):
     numcpus.set(int(numcpus.get()*2))
 def calculate1(*args):
-    numcpus.set(int(numcpus.get()/2)) 
-    
-    
-    
+    numcpus.set(int(numcpus.get()/2))
+
+
+
 def cluster_fiji(*args):
     timelim = s_t.get()
     memlim = s_mem.get()
     n_cpu = s_cpu.get()
-    
+
     callcmd = 'srun -N1 --pty --x11 n'
 
     callcmd += n_cpu
-    
+
     callcmd += ' --mem '+memlim+'G '
 
     callcmd += '-t 0-'+timelim+':00'
-    
+
     callcmd += 'bash /g/emcf/schorb/code/cluster/fijicluster.sh'
-    
-    
+
+
     os.system('cp /g/emcf/schorb/code/cluster/ImageJ.cfg  cp /g/emcf/schorb/code/cluster/ImageJ.cfg.orig')
-    
+
     os.system('echo -Xmx'+memlim+'g > /g/emcf/schorb/code/cluster/ImageJ.cfg')
     os.system('cat /g/emcf/schorb/code/cluster/ImageJ.cfg.orig >> /g/emcf/schorb/code/cluster/ImageJ.cfg')
-    
-    
+
+    print(callcmd)
+    root.destroy()
+    exit()
 
 
 
-     
-    
-    
+
+
+
 
 root = Tk()
 root.title("Cluster Parameters")
@@ -57,7 +59,7 @@ root.rowconfigure(0, weight=1)
 
 ttk.Label(mainframe, text="Set Parameters for Cluster Fiji").grid(column=2, row=1, sticky=W)
 
-numcpus = DoubleVar() 
+numcpus = DoubleVar()
 numcpus.set(2)
 s_cpu = Spinbox(mainframe, from_=1, to=100, textvariable=numcpus)
 s_cpu.grid(column=1, row=2, sticky=E)
@@ -66,7 +68,7 @@ s_cpu.grid(column=1, row=2, sticky=E)
 # ttk.Label(mainframe, text="Number of CPUs").grid(column=2, row=2, sticky=W)
 
 
-mem = DoubleVar() 
+mem = DoubleVar()
 mem.set(4)
 s_mem = Spinbox(mainframe, from_=1, to=128, textvariable=mem)
 s_mem.grid(column=1, row=3, sticky=E)
@@ -75,7 +77,7 @@ s_mem.grid(column=1, row=3, sticky=E)
 ttk.Label(mainframe, text="Memory (GB)").grid(column=2, row=3, sticky=W)
 
 
-time = DoubleVar() 
+time = DoubleVar()
 time.set(24)
 s_t = Spinbox(mainframe, from_=1, to=240, increment=6, textvariable=time)
 s_t.grid(column=1, row=4, sticky=E)
@@ -93,4 +95,3 @@ root.bind('<Return>', calculate)
 root.bind('<minus>', calculate1)
 
 root.mainloop()
-
