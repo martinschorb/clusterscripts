063de5071dfe5125cb788bab84df7655615ae7c7
Martin Schorb <martin.schorb@embl.de>
2020-10-15 10:31:51 +0200

    update groupsharepath

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Cluster tools for EMCF applications.
 includes Constantin's nice slurm submitter...
 
 [Instructions on how to start an interactive Fiji as a cluster job](doc/fiji.md)
+
+
+# Handy commands:
+
+- interactive cluster job:
+```srun -t 60:00 -N1 -n1 -W 0 --pty -E $SHELL```
--- groupsharepath.py
+++ groupsharepath.py
@@ -10,19 +10,20 @@ def groupsharepath(f1):
             p=subprocess.Popen('net use',stdout=subprocess.PIPE)
             n_use=p.communicate()[0].decode(encoding='ansi')
             drive=f1[:f1.find(':\\')+1]
-            
+
             if drive:
-                    shareline = n_use[n_use.find(drive):n_use.rfind('\\\ ')]
+                    shareline = n_use[n_use.find(drive):]
+                    shareline = shareline[:shareline.find('\n')]
                     shareline1 = shareline[shareline.rfind('\\')+1:]
-                    share = shareline1[:shareline1.find(' ')]		
+                    share = shareline1[:shareline1.find(' ')]
                     file1 = f1[f1.find(drive)+3:]
-                    
+
             else:
                     share1 = f1[f1.find('\\\\')+2:]
-                    share = share1[:share1.find('\\')]	
+                    share = share1[:share1.find('\\')]
                     f2 = f1[f1.rfind(share):]
                     file1 = f2[f2.find('\\')+1:]
-                    
+
             file1 = file1.replace('\\','/')
             file1 = '/g/'+share+'/'+file1
 
