a25e94d6e06f33fb2a4a71af1a0a2bc1ca043e31
Martin Schorb <martin.schorb@embl.de>
2022-02-22 07:14:49 +0000

    Update flattentrim.sh

--- flattentrim.sh
+++ flattentrim.sh
@@ -7,18 +7,14 @@ infile="$1";
 
 extension=$([[ "$infile" = *.* ]] && echo ".${infile##*.}" || echo '')
    
-base=$(basename "${infile}")
-directory=${infile%$base}
-	    
 base1=${base%$extension}
 
-base2=${base1%_full}
 echo 
 echo 
 echo -------   starting automatic flattening for $infile ----------------------
 echo 
 
-findsection -tomo $infile -si 20,1,20 -su flatten.mod -be c10_s1_hmfid.xyz
+findsection -tomo $infile -si 20,1,20 -su flatten.mod
 flattenwarp -i flatten.mod -ou flatten.tfm
 
 warpvol -sa -i $infile -ou "$base1"_flattened.rec -x flatten.tfm
@@ -34,6 +30,6 @@ findsection -tomo "$base1"_flattened.rec -si 20,1,20 > trim.out
 zlimits=`tail trim.out -n 1 | grep -o '[^:]\+$'`
 zlim1=${zlimits/  /,}
 
-trimvol -y $zlim1 -rx "$base1"_flattened.rec "$base2".rec
+trimvol -y $zlim1 -rx "$base1"_flattened.rec "$base1"_final.rec
 
 
